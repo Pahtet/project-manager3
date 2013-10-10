56ca30f0d0c099384de5bb9da576645b3327e15a
Pahtet <pahtet.nnov+git@gmail.com>
2013-10-10 15:52:37 +0400

    Deleted \n

--- project-manager/rhqdiffs.py
+++ project-manager/rhqdiffs.py
@@ -7,9 +7,9 @@ from managers.filemanager import FileManager
 from merger.merger import ProjectsMerger
 
 xmldir = 'D:\\merge-analysis\\'
-asmspath = "D:\\GitHub\\atlas\\kcc_appserver_ms\\RHQ460\\modules\\core\\"
-rhq46path = "D:\\GitHub\\atlas\\kcc_appserver_ms\\rhq-RHQ_4_6_0\\modules\\core\\"
-rhq49path =  "D:\\GitHub\\atlas\\kcc_appserver_ms\\RHQ490\\modules\\core\\"
+asmspath = "D:\\GitHub\\atlas\\kcc_appserver_ms\\RHQ460\\modules\\core\\domain\\"
+rhq46path = "D:\\GitHub\\atlas\\kcc_appserver_ms\\rhq-RHQ_4_6_0\\modules\\core\\domain\\"
+rhq49path =  "D:\\GitHub\\atlas\\kcc_appserver_ms\\RHQ490\\modules\\core\\domain\\"
 logs_path = "D:\\merge-analysis\\logs\\"
 
 inPath=asmspath 
@@ -35,18 +35,18 @@ if __name__=='__main__':
 	diffs = []
 	roots = []
 	diffs = comparator.get_diffs(True,inPath,outPath, diffs)
-	print("%d diffs founded\n"%len(diffs)) 	
+	print("%d diffs founded"%len(diffs)) 	
 	modified = get_modified(diffs)
-	print("%d modified diffs founded\n"%len(modified))	
+	print("%d modified diffs founded"%len(modified))	
 	new_files=get_new(diffs)
-	print("%d new files founded\n"%len(new_files))	
+	print("%d new files founded"%len(new_files))	
 
 	for mod in modified: 
 		roots = comparator.get_root_imports(mod,inPath,outPath,roots)
 	
 	for root in roots:
 		file_manager.writetoxmlfile(xmldir+root.name+'.xml', root.xmlprint('<?xml version="1.0"?>\n'))
-	print("%d roots\n"%len(roots)) 
+	print("%d roots"%len(roots)) 
 
 	merge=ProjectsMerger(rhq46path,rhq49path,logs_path)
 	
